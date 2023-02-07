50518014fea5bbdcf34b7ddacd6044769943c8d8
b-sandrine <sanrdushimimana@gmail.com>
2023-02-07 21:31:25 +0200

    Added a simple logic

--- quote.js
+++ quote.js
@@ -1,2 +1,13 @@
 
-var document = document.getElementById("root");
+var quote_holder = document.getElementById("root");
+var quote_author = document.getElementById("author");
+
+let quotes = 
+    {
+        "quote": "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        "author": "Nelson Mandela",
+    }
+
+
+quote_holder.innerText = quotes.quote;
+quote_author.innerText = quotes.author;
